ee92edbf8839f549b9f69cf530a79d850f3809b6
Jetross <jetrossneri07@gmail.com>
2026-03-04 11:25:57 +0800

    feat: add themes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,9 @@
+import { ModeToggle } from "@/components/theme-toggle";
+
 export default function Home() {
-  return <div className="min-h-screen bg-background"></div>;
+  return (
+    <div className="min-h-screen bg-background">
+      <ModeToggle />
+    </div>
+  );
 }
